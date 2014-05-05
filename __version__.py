15865cb8a7cc3d0608d60f6a4647aa7dd6f8e3b2
rmichie <michie.ryan@deq.state.or.us>
2014-05-05 03:16:53 -0700

    updated to v900b7

--- __version__.py
+++ __version__.py
@@ -39,6 +39,6 @@ major = 9
 minor = 0
 micro = 0
 prerel = 'b'
-prerelversion = '6' 
+prerelversion = '7' 
 version_info = (major,minor,micro,prerel,prerelversion)
 version_string = "%s.%s.%s%s%s" % version_info
