6f5f73aaba34248effcd3b142f0a84afb3028899
rmichie <michie.ryan@deq.state.or.us>
2014-08-17 22:58:12 -0700

    fixed msgbox title

--- HS9_py3/heatsource9/BigRedButton.py
+++ HS9_py3/heatsource9/BigRedButton.py
@@ -228,7 +228,7 @@ def RunHS(inputdir, control_file):
         f = open("HS_Error.txt", "w")
         print_exc(file=f)
         f.close()
-        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s" % stderr, "HeatSource Error")
+        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s" % stderr, "Heat Source Error")
 def RunSH(inputdir, control_file):
     """Run solar routines only"""
     try:
@@ -238,7 +238,7 @@ def RunSH(inputdir, control_file):
         f = open("HS_Error.txt", "w")
         print_exc(file=f)
         f.close()
-        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s"%stderr, "HeatSource Error")
+        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s"%stderr, "Heat Source Error")
 def RunHY(inputdir, control_file):
     """Run hydraulics only"""
     try:
@@ -248,7 +248,7 @@ def RunHY(inputdir, control_file):
         f = open("HS_Error.txt", "w")
         print_exc(file=f)
         f.close()
-        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s"%stderr, "HeatSource Error")
+        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s"%stderr, "Heat Source Error")
 
 def RunSetup(inputdir,control_file):
     """Run setup"""
@@ -259,5 +259,5 @@ def RunSetup(inputdir,control_file):
         f = open("HS_Error.txt", "w")
         print_exc(file=f)
         f.close()
-        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s"%stderr, "HeatSource Error")        
+        msgbox("".join(format_tb(exc_info()[2]))+"\nSynopsis: %s"%stderr, "Heat Source Error")        
         
