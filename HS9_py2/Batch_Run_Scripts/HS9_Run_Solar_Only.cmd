d77f70aeaf4df680c2e9977d6c90339f0f8b072f
rmichie <michie.ryan@deq.state.or.us>
2016-04-02 11:51:57 -0700

    Remove batch scripts directory

--- HS9_py2/Batch_Run_Scripts/HS9_Run_Solar_Only.cmd
+++
@@ -1,9 +0,0 @@
-:: DO NOT DELETE THIS FILE. This script opens command line and runs the named script.
-:: This script must be located in the same directory as HeatSource_control.csv.
-:: -i keeps command line open when the script is finished.
-
-@echo off
-python -i HS9_Run_Solar_Only.py
-
-
-
--- HS9_py2/Batch_Run_Scripts/HS9_Setup_Control_File.cmd
+++
@@ -1,8 +0,0 @@
-:: DO NOT DELETE THIS FILE. This script opens command line and runs the named script.
-:: This script must be located in the same directory as HeatSource_control.csv.
-:: -i keeps command line open when the script is finished.
-
-@echo off
-python -i HS9_Setup_Control_File.py
-
-
