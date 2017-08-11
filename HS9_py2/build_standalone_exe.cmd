0e8d58f3d13c2c3fd93dd1d0147a2c3690a9e070
rmichie <michie.ryan@deq.state.or.us>
2017-08-11 14:05:31 -0700

    Adding build script
    Script to build exectubles for windows using pyinstaller.

---
+++ HS9_py2/build_standalone_exe.cmd
@@ -0,0 +1,12 @@
+@echo OFF
+
+:: This script builds heat source exectubles for windows 
+:: Requires pyinstaller
+pyinstaller -F HS9_Run_Hydraulics_Only.py
+pyinstaller -F HS9_Run_Solar_Only.py
+pyinstaller -F HS9_Run_Temperature.py
+pyinstaller -F HS9_Setup_Control_File.py
+echo .
+echo .
+echo 'dist' folder contains exectubles
+pause
