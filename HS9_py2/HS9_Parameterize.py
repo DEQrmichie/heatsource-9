d0bc12fc4c7e0341f4a89300cff3df6d90f44ed5
rmichie <michie.ryan@deq.state.or.us>
2017-11-15 16:29:53 -0800

    updated with arc functions
    Moved functions that require ESRI arcpy here so they aren't in the main model.

--- HS9_py2/HS9_Parameterize.py
+++ HS9_py2/HS9_Parameterize.py
@@ -15,12 +15,136 @@ by typing:  python -i path/to/this/script/HS9_Run_Parameterize.py
 from heatsource9.ModelSetup.Inputs import Inputs
 from heatsource9.Dieties.IniParamsDiety import IniParams
 from heatsource9.Dieties.IniParamsDiety import dtype
+from collections import defaultdict
 from os.path import abspath
 from os.path import dirname
 from os.path import exists
+from os.path import isfile
 from os.path import join
 from os.path import realpath
 
+def parameterize_from_nodes_fc(input_file, nodes_fc,
+                               group_val=None,
+                               grouping_field="STREAM_ID",
+                               cont_stream_km=False, overwrite=False):
+    """
+    Parameterize the input file using data from the TTools
+    node feature class.
+
+    input_file: The input to be parameterized. Can be either:
+    "lcdatafile" or "morphfile". Other inputs must use
+    parameterize() with the input data supplied as a list.
+
+    nodes_fc: TTools derived point feature class
+
+    group_val: Unique ID to group into a model instance.
+
+    grouping_field: the attribute field in the nodes_fc that
+    contains the group_val.
+
+    cont_stream_km: if True overwrites the nodes km so it is
+    continuous based on node_dx over all the nodes in each group
+
+    overwrite: if True overwrites an existing file.
+
+    """
+
+    msg = "Parameterize from nodes fc"
+    print(msg)
+
+    # try to import arcpy
+    try:
+        import arcpy
+    except:
+        msg = "ImportError: No module named arcpy. To use this method ESRI ArcGIS must be installed"
+        print(msg)
+        SystemExit
+
+    # get the headers
+    if input_file == "lcdatafile":
+        headers = inputs.headers_lcdata()
+    elif input_file == "morphfile":
+        headers = inputs.headers_morph()
+    else:
+        print("input_file = {0}, should be \
+        'lcdatafile' or 'morphfile'".format(input_file))
+        SystemExit
+
+    # check to see if the file exists before moving on
+    if not overwrite and isfile(join(IniParams["inputdir"],
+                                     IniParams[input_file])):
+        msg = "{0} already exists and will not be overwritten".format(IniParams[input_file])
+        print(msg)
+        return
+
+    # Get all the column headers in the nodes fc
+    nodes_fc_headers = [field.name for field in arcpy.Describe(nodes_fc).fields]
+
+    # find the unique values in the grouping field
+    whereclause = """{0} = '{1}'""".format(grouping_field,
+                                           group_val)
+
+    # read the node fc into the nodes dict
+    nodeDict = read_nodes_fc(nodes_fc, nodes_fc_headers, whereclause)
+
+    # build a list of the data to pass to parameterize()
+    outlist = []
+    nodeIDs = nodeDict.keys()
+    nodeIDs.sort()
+
+    if cont_stream_km:
+        kmlist= inputs.stream_kms()
+
+    for i, nodeID in enumerate(nodeIDs):
+        row_list = []
+        for header in headers:
+            if header in nodeDict[nodeID].keys():
+                val = nodeDict[nodeID][header]
+                if cont_stream_km and header == "STREAM_KM":
+                    row_list.append(kmlist[i])
+                else:
+                    row_list.append(val)
+            else:
+                # use None when there is no matching key
+                row_list.append(None)
+        outlist.append(row_list)
+
+    # sort by stream km with headwaters on top
+    outlist = sorted(outlist, key=itemgetter(2), reverse=True)
+
+    inputs.write_to_csv(IniParams["inputdir"],
+                        IniParams[input_file],
+                        outlist, headers)
+
+def read_nodes_fc(nodes_fc, readfields, whereclause):
+    """
+    Reads an input point file and returns the fields as a
+    nested dictionary
+    """
+    # try to import arcpy
+    try:
+        import arcpy
+    except:
+        msg = "ImportError: No module named arcpy. To use this method ESRI ArcGIS must be installed"
+        print(msg)
+        SystemExit
+
+    nodeDict = nested_dict()
+    incursorFields = ["NODE_ID"] + readfields
+    # Determine input point spatial units
+    proj = arcpy.Describe(nodes_fc).spatialReference
+
+    with arcpy.da.SearchCursor(nodes_fc, incursorFields, whereclause, proj) as Inrows:
+        for row in Inrows:
+            for f in xrange(0,len(readfields)):
+                nodeDict[row[0]][readfields[f]] = row[1+f]
+    return nodeDict
+
+def nested_dict(): 
+    """
+    Build a nested dictionary
+    """
+    return defaultdict(nested_dict)
 
 def getScriptPath():
     """Gets the path to the directory where this script is
@@ -92,13 +216,13 @@ inputs.setup(use_timestamp=False, overwrite=True)
 # from nodes feature class. 
 # NOTE this method currently requires use of arcpy and an active 
 # ArcGIS Desktop license
-inputs.parameterize_from_nodes_fc(input_file="lcdatafile", nodes_fc=nodes_fc,
-                                  group_val="Example Model", grouping_field="STREAM_ID",
-                                  cont_stream_km=False, overwrite=False)
+parameterize_from_nodes_fc(input_file="lcdatafile", nodes_fc=nodes_fc,
+                           group_val="Example Model", grouping_field="STREAM_ID",
+                           cont_stream_km=False, overwrite=False)
 
-inputs.parameterize_from_nodes_fc(input_file="morphfile", nodes_fc=nodes_fc,
-                                  group_val="Example Model", grouping_field="STREAM_ID",
-                                  cont_stream_km=False, overwrite=False)
+parameterize_from_nodes_fc(input_file="morphfile", nodes_fc=nodes_fc,
+                           group_val="Example Model", grouping_field="STREAM_ID",
+                           cont_stream_km=False, overwrite=False)
 
 # Parameterize the lccodes input 
 lccodes = [('Active River Channel',100,0,0,0), 
