186a3d860216583422535df3001b871148becb8d
rmichie <michie.ryan@deq.state.or.us>
2018-04-02 15:15:18 -0700

    Added model inputs setup script

--- HS9_py2/build_standalone_exe.cmd
+++ HS9_py2/build_standalone_exe.cmd
@@ -6,6 +6,7 @@ pyinstaller -F HS9_Run_Hydraulics_Only.py
 pyinstaller -F HS9_Run_Solar_Only.py
 pyinstaller -F HS9_Run_Temperature.py
 pyinstaller -F HS9_Setup_Control_File.py
+pyinstaller -F HS9_Setup_Model_Inputs.py
 echo .
 echo .
 echo 'dist' folder contains exectubles
