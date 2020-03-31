50216e4c150dbe0fea43ca102e33518eb55e60da
rmichie <michie.ryan@deq.state.or.us>
2020-03-31 09:36:22 -0700

    Update install_wheel.cmd

--- install_wheel.cmd
+++ install_wheel.cmd
@@ -2,5 +2,5 @@
 
 :: This script installs the heatsource wheel for windows
 
-pip install --upgrade --force-reinstall dist/heatsource9-9.0.0b24-cp27-cp27m-win32.whl
+pip2 install --upgrade --force-reinstall dist/heatsource9-9.0.0b25-cp27-cp27m-win32.whl
 pause
