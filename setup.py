33ac21bd685e1b2454cb541c8857604b985ae7ef
rmichie <michie.ryan@deq.state.or.us>
2020-04-21 20:51:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(name='heatsource9',
                 'heatsource9.Stream',
                 'heatsource9.Utils'],
       package_dir={'': 'src'},
-      install_requires=['Cython'],
+      install_requires=['Cython==0.29.16'],
       ext_modules=extensions,
       python_requires='==2.7.*'
       )
