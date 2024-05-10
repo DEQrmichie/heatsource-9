459113d1769682ec068b450db61a8870bc6e33f1
DEQrmichie <65993167+DEQrmichie@users.noreply.github.com>
2024-05-09 21:25:49 -0700

    add read/write from excel

--- exe/hs9_run_solar.py
+++ exe/hs9_run_solar.py
@@ -33,6 +33,9 @@ from os.path import dirname
 from os.path import exists
 from os.path import join
 from os.path import realpath
+from os.path import split
+from os.path import splitext
+from glob import glob
 
 if getattr(sys, 'frozen', False):
     # path to the directory where the exe is being executed from
@@ -42,12 +45,24 @@ else:
     application_path = abspath(join(dirname(realpath(__file__)), '.'))
 
 model_dir = join(application_path, '')
-control_file = 'HeatSource_Control.csv'
+full_path = glob(join(model_dir, "HeatSource_Control.*"))
 
-if not exists(join(model_dir, control_file)):
-    raise Exception("HeatSource_Control.csv not found. \
-    Move the executable or place the control file in \
-    this directory: {0}.".format(model_dir))
+# checks to make sure HeatSource_Control.xlsx or HeatSource_Control.csv exists
+if len(full_path) == 0:
+    raise Exception("HeatSource_Control file not found. \
+        Move the executable or place the control file in \
+        this directory: {0}.".format(model_dir))
+
+if len(full_path) > 1:
+    raise Exception("There is more than one file named 'HeatSource_Control.' \
+        in this directory: {0}. \
+        Only one file can exist.".format(model_dir))
+
+control_file = split(full_path[0])[1]
+control_ext = splitext(control_file)[1]
+
+if control_ext not in [".xlsx", ".csv"]:
+    raise Exception("{0} must be an Excel '.xlsx' or '.csv' file.".format(control_file))
 
 # Run Heat Source Solar only, run_type = 1
 BigRedButton.run_solar(model_dir, control_file)
