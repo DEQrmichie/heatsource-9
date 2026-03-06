9a48182d0c99f81cd0974a313b300a3f2642c4f7
Ryan Michie <ryan.michie@deq.oregon.gov>
2026-03-05 21:31:27 -0800

    framing

---
+++ src/heatsource9/__init__.py
@@ -0,0 +1,4 @@
+# heatsource9/__init__.py
+from heatsource9.__version__ import __version__
+
+__all__ = ["__version__"]
---
+++ src/heatsource9/domain/__init__.py
@@ -0,0 +1,2 @@
+# heatsource9/domain/__init__.py
+"""The domain subpackage holds the model simulation container and time progression objects."""
